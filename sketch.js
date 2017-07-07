b3a74c1c80c6ff2c61061a66e12b499a0e334183
MurugeshDC <murugesh2059@gmail.com>
2017-07-07 16:16:17 +0530

    Clear the sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-	for(var i=0;i<15;i++) {
-		$('#table').append("<tr><td></td></tr>");
-	}
-	
-})
+
+    for(var i=0;i<15;i++) {
+        $('#table').append("<tr>");
+        for (var j = 0; j < 15; j++) {
+            $('#table').find('tr').last().append("<td></td>");
+        }
+        $('#table').append("</tr>");
+    }
+
+    $("td").hover(function() {
+        $(this).addClass("red");
+    });
+
+    $("#clear").on("click",function() {
+        $("td").removeClass("red");
+    });
+
+
+
+});
